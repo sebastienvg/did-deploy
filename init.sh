5e291761e7c9bb2f7bdbb54dc58d0190451281b7
Sebastien Vayrette-Gavard <Sebastien.Vayrette-Gavard@amdocs.com>
2018-09-22 18:05:21 -0400

    more logic within init

--- init.sh
+++ init.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
-## if you pass --base flag it will install/reinstall postgres data
-# decrypt things
+
+## stop the containers if they are already running
+stop -deploy-server.sh
+## decrypt files
 ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml
 
-		if [ $1 == --base ]
-		then
-			tar xzf pgdata.tar.gz
-		fi
+## if deploy-vault.pass is empty or if content is not good EXIT
+deploypass=$(md5 deploy-vault.pass)
 
-docker-compose up -d
+if [ $deploypass ==  68b329da9893e34099c7d8ad5cb9c940]
+	then
+		echo "You don't have a valid licence"
+else
+	## if you pass --base flag it will install/reinstall postgres data 
+	if [ $1 == --base ]
+                then
+			ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml
+                        tar xzf pgdata.tar.gz
+                fi	
+        ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml
+	./start-deploy-server.sh
+fi
 
-echo "You should be able to login to http://localhost:8053 with admin/admin"
+## if you want to start without data reset
+if [ $1 == --start]
+	then
+		./start-deploy-server.sh
+fi
+
+
+#./start-deploy-server.sh
+
+#echo "You should be able to login to http://localhost:8053 with admin/admin"
 
