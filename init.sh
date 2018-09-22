2344468410eafe51893d7a3fab9ed58fa2c0e146
Sebastien Vayrette-Gavard <Sebastien.Vayrette-Gavard@amdocs.com>
2018-09-22 17:41:49 -0400

    add some logic within init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,19 @@
-mkdir awx_projects
-ansible-vault decrypt --vault-password-file=deploy-vault.pass
+#!/bin/bash
+# decrypt things
+ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml
+
+
+
+		if [ $1 == 1 ]
+		then
+			tar xzf pgdata.tar.gz
+		fi
+
+
+
+
+
+docker-compose up -d
+
+echo "You should be able to login to http://localhost:8053 with admin/admin"
+
