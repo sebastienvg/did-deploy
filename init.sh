63fb5b67c0f85683c4963ef0d220585544ed8d4f
Sebastien Vayrette-Gavard <Sebastien.Vayrette-Gavard@amdocs.com>
2018-09-22 17:47:33 -0400

    adding option to init.sh

--- init.sh
+++ init.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
+## if you pass --base flag it will install/reinstall postgres data
 # decrypt things
 ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml
 
-
-
-		if [ $1 == 1 ]
+		if [ $1 == --base ]
 		then
 			tar xzf pgdata.tar.gz
 		fi
-
-
-
-
 
 docker-compose up -d
 
