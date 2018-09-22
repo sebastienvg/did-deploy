3ddfd24422ee14e5100480f5f8e11123d1f06e2e
Sebastien Vayrette-Gavard <Sebastien.Vayrette-Gavard@amdocs.com>
2018-09-22 19:18:12 -0400

    minor change to init.sh

--- init.sh
+++ init.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
 ## stop the containers if they are already running
-./stop-deploy-server.sh	2>/dev/null
+#./stop-deploy-server.sh	2>/dev/null
 ## decrypt files
-ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml 2>/dev/null
+#ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml stop-deploy-server.sh start-deploy-server.sh 2>/dev/null
 
 ## if deploy-vault.pass is empty or if content is not good EXIT
 deploypass=$(cat deploy-vault.pass)
 
-echo "key is $deploypass"
+#echo "key is $deploypass"
 
-if [ $deploypass == x ]
+if  [ '$deploypass' == x ]
 	then
 		echo "You don't have a valid licence"
 		exit
-else
-	## if you pass --base flag it will install/reinstall postgres data 
-	if [ $1 == --base ]
+elif [ '$deploypass' != x ]
+then
+	## if you pass --base flag it will install/reinstall postgres data
+	if [ $1 == '--base' ]
                 then
 			ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml stop-deploy-server.sh start-deploy-server.sh 2>/dev/null
                         tar xzf pgdata.tar.gz
-                fi	
+                fi
         ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml stop-deploy-server.sh start-deploy-server.sh 2>/dev/null
-	./start-deploy-server.sh 
+	 sh start-deploy-server.sh
+echo "You should be able to login to http://localhost:8053 with admin/admin"
 fi
 
 ## if you want to start without data reset
-if [ $1 == --start ]
+if [ $1 == '--start' ]
 	then
-		./start-deploy-server.sh
+		sh start-deploy-server.sh
+echo "You should be able to login to http://localhost:8053 with admin/admin"
 fi
 
-
-#./start-deploy-server.sh
-
-#echo "You should be able to login to http://localhost:8053 with admin/admin"
 
