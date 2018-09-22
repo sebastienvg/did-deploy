4a4508d4445d5344649f61c654903eeeeb7c43a5
Sebastien Vayrette-Gavard <Sebastien.Vayrette-Gavard@amdocs.com>
2018-09-22 19:38:08 -0400

    cleanup init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-## stop the containers if they are already running
-#./stop-deploy-server.sh	2>/dev/null
-## decrypt files
-#ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml stop-deploy-server.sh start-deploy-server.sh 2>/dev/null
-
-## if deploy-vault.pass is empty or if content is not good EXIT
 deploypass=$(cat deploy-vault.pass)
-
-#echo "key is $deploypass"
 
 if  [ '$deploypass' == x ]
 	then
@@ -19,19 +11,18 @@ then
 	## if you pass --base flag it will install/reinstall postgres data
 	if [ $1 == '--base' ]
                 then
-			ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml stop-deploy-server.sh start-deploy-server.sh 2>/dev/null
+			ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz  2>/dev/null
+		echo "pgdata decrypted"
+			ansible-vault decrypt --vault-password-file=deploy-vault.pass docker-compose.yml  2>/dev/null
+		echo "compose file decrypted"
+			#ansible-vault decrypt --vault-password-file=deploy-vault.pass stop-deploy-server.sh  2>/dev/null
+			ansible-vault decrypt --vault-password-file=deploy-vault.pass start-deploy-server.sh 2>/dev/null
+		echo "unpacking"
                         tar xzf pgdata.tar.gz
+	 	        sh start-deploy-server.sh
                 fi
-        ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml stop-deploy-server.sh start-deploy-server.sh 2>/dev/null
+#        ansible-vault decrypt --vault-password-file=deploy-vault.pass pgdata.tar.gz docker-compose.yml stop-deploy-server.sh start-deploy-server.sh 2>/dev/null
 	 sh start-deploy-server.sh
 echo "You should be able to login to http://localhost:8053 with admin/admin"
 fi
-
-## if you want to start without data reset
-if [ $1 == '--start' ]
-	then
-		sh start-deploy-server.sh
-echo "You should be able to login to http://localhost:8053 with admin/admin"
-fi
-
 
